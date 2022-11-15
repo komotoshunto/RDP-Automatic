f800b2d6bc2269cb1a8a9d509da617ffc1476a8c
shun <78147485+komotoshunto@users.noreply.github.com>
2022-11-15 14:26:46 +0900

    Update その屋根に雨が最も当たる角度を抽出する.py

--- 関数/その屋根に雨が最も当たる角度を抽出する.py
+++ 関数/その屋根に雨が最も当たる角度を抽出する.py
@@ -1,20 +1,16 @@
 #Input
-#angle_rainfall, Item Access, float
-#roof_list, List Access, Mesh
-#barrier, List Access, GeometryBese
-
+#rain_angle, Item Access, float
+#mesh_list, List Access, Mesh
 
 #Output
-#ans_vec, 
-#intersect_list, 
+#AnswerVector_list, 
 
 
 import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
+import ghpythonlib.components as ghcomp
 import math
-import System
-import Grasshopper.Kernel.Data.GH_Path as gh_path
-import Grasshopper.DataTree as datatree
+
 
 #各角度を定義する
 max_angle = int(rain_angle)
@@ -26,6 +22,8 @@ axis2 = rg.Vector3d(0.0, 0.0, 1.0)
 for i in range(min_angel, max_angle + 1, step):
     for k in range(0, 360, step):
         if i == 0:
+            vec = rg.Vector3d(0.0, 0.0, 1.0)
+            vec_list.append(vec)
             break
         else:
             angleRad1 = math.radians(i)
@@ -34,42 +32,34 @@ for i in range(min_angel, max_angle + 1, step):
             re_vec1 = rg.Vector3d.Rotate(vec, angleRad1, axis1)
             re_vec2 = rg.Vector3d.Rotate(vec, angleRad2, axis2)
             vec_list.append(vec)
-def max_projected_area(mesh, barrier):
-    #重心を求める
+
+AnswerVector_list = []
+#重心を求める
+for num, mesh in enumerate(mesh_list):
+    meshfacelist = mesh.Faces
     centroid = []
-    mfl = mesh.Faces
-    for h in range(len(mfl)):
-        center = rg.Collections.MeshFaceList.GetFaceCenter(mfl, h)
+    for h in range(len(meshfacelist)):
+        center = rg.Collections.MeshFaceList.GetFaceCenter(meshfacelist, h)
         centroid.append(center)
     
-    #光線を発射
-    ans_count = float("inf")
-    for dir in vec_list:
-        count = 0
-        intersect = []
-        for point in centroid:
-            ray = rg.Ray3d(point, dir)
-            rayshootevent = rg.Intersect.Intersection.RayShoot(barrier, ray, 1)
-            count += len(rayshootevent)
-            if len(rayshootevent) == 0:
-                intersect.append(True)
-            else:
-                intersect.append(False)
-        if count < ans_count:
-            ans_count = count
-            ans_vec = dir
-            intersect_list = intersect
+    #ProjectPointに引っかからないように照射点と同じ位置にあるmeshを削除する
+    add = mesh
+    mesh_list.pop(num)
     
-    return intersect_list, ans_vec
-
-
-tree1 = datatree[System.Object]()
-tree2 = datatree[System.Object]()
-for num, roof in enumerate(roof_list):
-    intersect, vec = max_projected_area(roof, barrier)
-    print(num, intersect)
-    path = gh_path(num)
-    tree1.Add(vec, path)
-    tree2.Add(intersect, path)
-ans_vec = tree1
-intersect_list = tree2
+    ans_cou = float("inf")
+    for vec in vec_list:
+        projectpoint = ghcomp.ProjectPoint(centroid, vec, mesh_list)
+        if projectpoint[0] == None:
+            count = 0
+            ans_vec = vec
+            break
+        else:
+            count = len(projectpoint[0])
+            cou = count
+            if cou < ans_cou:
+                ans_cou = cou
+                ans_vec = vec
+    AnswerVector_list.append(ans_vec)
+    
+    #消したmeshを元に戻す
+    mesh_list.insert(num, add)
