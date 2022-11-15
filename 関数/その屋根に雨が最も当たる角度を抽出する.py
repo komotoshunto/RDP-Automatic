14c7d7c9c5715a2096ea914aa1a3a6e6a7aabf0b
shun <78147485+komotoshunto@users.noreply.github.com>
2022-11-15 15:36:58 +0900

    Update その屋根に雨が最も当たる角度を抽出する.py

--- 関数/その屋根に雨が最も当たる角度を抽出する.py
+++ 関数/その屋根に雨が最も当たる角度を抽出する.py
@@ -35,50 +35,53 @@ for i in range(min_angel, max_angle + 1, step):
             re_vec2 = rg.Vector3d.Rotate(vec, angleRad2, axis2)
             vec_list.append(vec)
 
-
-centroid_list = []
-collision_list = []
-AnswerVector_list = []
-#重心を求める
-for num, mesh in enumerate(mesh_list):
-    meshfacelist = mesh.Faces
-    centroid = []
-    for h in range(len(meshfacelist)):
-        center = rg.Collections.MeshFaceList.GetFaceCenter(meshfacelist, h)
-        centroid.append(center)
-        centroid_list.append(center)
-    
-    #ProjectPointに引っかからないように照射点と同じ位置にあるmeshを削除する
-    add = mesh
-    mesh_list.pop(num)
-    
-    ans_cou = float("inf")
-    ans_collision = []
-    for vec in vec_list:
-        projectpoint = ghcomp.ProjectPoint(centroid, vec, mesh_list)
-        if projectpoint[0] == None:
-            ans_vec = vec
-            if isinstance(projectpoint[1], int):
-                ans_collision = [projectpoint[1]]
-            else:
-                ans_collision = projectpoint[1]
-            break
-        else:
-            count = len(projectpoint[0])
-            cou = count
-            if cou < ans_cou:
-                ans_cou = cou
+def max_RainAngle_list(mesh_list):
+    centroid_list = []
+    collision_list = []
+    AnswerVector_list = []
+    #重心を求める
+    for num, mesh in enumerate(mesh_list):
+        meshfacelist = mesh.Faces
+        centroid = []
+        for h in range(len(meshfacelist)):
+            center = rg.Collections.MeshFaceList.GetFaceCenter(meshfacelist, h)
+            centroid.append(center)
+            centroid_list.append(center)
+        
+        #ProjectPointに引っかからないように照射点と同じ位置にあるmeshを削除する
+        add = mesh
+        mesh_list.pop(num)
+        
+        ans_cou = float("inf")
+        ans_collision = []
+        for vec in vec_list:
+            projectpoint = ghcomp.ProjectPoint(centroid, vec, mesh_list)
+            if projectpoint[0] == None:
                 ans_vec = vec
-                ans_collision = projectpoint[1]
-    
-    for i in ans_collision:
-        if i == -1:
-            collision_list.append(True)
-        else:
-            collision_list.append(False)
-    
-    for k in range(len(centroid)):
-        AnswerVector_list.append(ans_vec)
-    
-    #消したmeshを元に戻す
-    mesh_list.insert(num, add)
+                if isinstance(projectpoint[1], int):
+                    ans_collision = [projectpoint[1]]
+                else:
+                    ans_collision = projectpoint[1]
+                break
+            else:
+                count = len(projectpoint[0])
+                cou = count
+                if cou < ans_cou:
+                    ans_cou = cou
+                    ans_vec = vec
+                    ans_collision = projectpoint[1]
+        
+        for i in ans_collision:
+            if i == -1:
+                collision_list.append(True)
+            else:
+                collision_list.append(False)
+        
+        for k in range(len(centroid)):
+            AnswerVector_list.append(ans_vec)
+        
+        #消したmeshを元に戻す
+        mesh_list.insert(num, add)
+    return centroid_list, AnswerVector_list, collision_list
+
+centroid_list, AnswerVector_list, collision_list = max_RainAngle_list(mesh_list)
