75f4f89e10be5a6da20fb4ddcec7f59b4a35cf10
shun <78147485+komotoshunto@users.noreply.github.com>
2022-11-21 15:05:21 +0900

    Update Meshの各FaceをSurfaceにする.py

--- 参考API/Meshの各FaceをSurfaceにする.py
+++ 参考API/Meshの各FaceをSurfaceにする.py
@@ -10,16 +10,29 @@ import Rhino.Geometry as rg
 
 
 #Meshの各FaceをSurfaceにする関数
+#Meshの各FaceをSurfaceにする関数
 def SurfaceFromMesh(mesh):
     surface_list = []
     edges = ghcomp.FaceBoundaries(mesh)
-    for edge in edges:
-        explode = ghcomp.Explode(edge, True)
+    print(edges)
+    if isinstance(edges, list):
+        for edge in edges:
+            explode = ghcomp.Explode(edge, True)
+            segment = explode[0]
+            if len(segment) == 4:
+                surface = ghcomp.EdgeSurface(segment[0], segment[1], segment[2], segment[3])
+            elif len(segment) == 3:
+                surface = ghcomp.EdgeSurface(segment[0], segment[1], segment[2])
+            surface_list.append(surface)
+    else:
+        explode = ghcomp.Explode(edges, True)
         segment = explode[0]
         if len(segment) == 4:
             surface = ghcomp.EdgeSurface(segment[0], segment[1], segment[2], segment[3])
         elif len(segment) == 3:
             surface = ghcomp.EdgeSurface(segment[0], segment[1], segment[2])
         surface_list.append(surface)
+    return surface_list
+
 
 a = SurfaceFromMesh(mesh)
