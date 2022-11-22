5cea5510810f8a9ef3495a539e872eb3e06d5b35
shun <78147485+komotoshunto@users.noreply.github.com>
2022-11-22 09:48:22 +0900

    Update 一時的にリストから削除して元に戻す.py

--- 参考API/一時的にリストから削除して元に戻す.py
+++ 参考API/一時的にリストから削除して元に戻す.py
@@ -1,25 +1,21 @@
 list = [430, 10, 53, 43, 4, 50]
 pop_index_list = [1, 2, 1, 1, 2, 2]
+print("削除前", list)
 
 
 val = 1 #pop_index_listと同じ数字をlistから削除する
 pop_list = [i for i, x in enumerate(pop_index_list) if x == val]
-print("削除前", list)
+add_list = []
+for add_num in sorted(pop_list, reverse=False):
+    add = list[add_num]
+    add_list.append(result)
 
 
-add = []
-for k in sorted(pop_list, reverse=False):
-    result = list[k]
-    add.append(result)
-
-
-
-for i in sorted(pop_list, reverse=True):
-    list.pop(i)
+for pop_num in sorted(pop_list, reverse=True):
+    list.pop(pop_num)
 print("削除後", list)
 
 
-
-for num, j in enumerate(sorted(pop_list, reverse=False)):
-    list.insert(j, add[num])
+for restore_num, restore_val in enumerate(sorted(pop_list, reverse=False)):
+    list.insert(restore_val, add_list[restore_val])
 print("修正後", list)
