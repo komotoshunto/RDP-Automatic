587f92e5e8ac556e9148fb4be22cbe29b26059e0
shun <78147485+komotoshunto@users.noreply.github.com>
2022-11-22 12:06:51 +0900

    Update 法線方向から面を流れる方向を求める.py

--- 関数/法線方向から面を流れる方向を求める.py
+++ 関数/法線方向から面を流れる方向を求める.py
@@ -7,12 +7,14 @@
 
 #法線方向から面を流れる方向を求める関数
 def FlowVector(normal_vec):
-    x = normal_vec[0]
-    y = normal_vec[1]
+    print(normal_vec[2])
     if normal_vec[2] == 0:
         z = -1
     else:
-        z = -(x**2 + y**2) / normal_vec[2]
+        z = -1 * ((normal_vec[0]**2 + normal_vec[1]**2)**(1/2))
+    k = (-1) * ((normal_vec[2] * z) / (normal_vec[0]**2 + normal_vec[1]**2))
+    x = normal_vec[0] * k
+    y = normal_vec[1] * k
     flow_vec = rg.Vector3d(x, y, z)
     return flow_vec
 
