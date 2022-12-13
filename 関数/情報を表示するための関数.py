c55cb8ebd8092ba2998ab7431e8c8150faa6c917
shun <78147485+komotoshunto@users.noreply.github.com>
2022-12-13 17:17:44 +0900

    Create 情報を表示するための関数.py

---
+++ 関数/情報を表示するための関数.py
@@ -0,0 +1,15 @@
+#Rhino上で情報を表示するための関数
+def pipe_cou_display(total_area, method, NoLeak_PipeCount_dic, Leak_PipeCount_dic):
+    pipe_inf = ["計算方法:" + str(method),
+                "合計の受持ち面積(㎡):" + str(total_area),
+                "侵入の恐れがない場合(本)",
+                "A100:" + str(NoLeak_PipeCount_dic["A100"]),
+                "A125:" + str(NoLeak_PipeCount_dic["A125"]),
+                "A150:" + str(NoLeak_PipeCount_dic["A150"]),
+                "A200:" + str(NoLeak_PipeCount_dic[""]),
+                "侵入の恐れがある場合(本)",
+                "A100:" + str(Leak_PipeCount_dic["A100"]),
+                "A125:" + str(Leak_PipeCount_dic["A125"]),
+                "A150:" + str(Leak_PipeCount_dic["A150"]),
+                "A200:" + str(Leak_PipeCount_dic["A200"])]
+    return pipe_inf
